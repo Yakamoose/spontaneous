6f073eae62818e6f243372032fce56bbffc779f8
Eric J <xericx.johnson@gmail.com>
2018-03-31 13:56:58 -0700

    first test up and running

--- server.js
+++ server.js
@@ -1,4 +1,53 @@
+'use strict';
+
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+const { PORT, DATABASE_URL, TEST_DATABASE_URL } = require('./config');
+
 const app = express();
 app.use(express.static('public'));
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
+
+let server;
+
+function runServer(databaseUrl, port = PORT) {
+
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer() {
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
---
+++ test/start-up-test.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const mongoose = require('mongoose');
+
+const expect = chai.expect;
+
+const {app, runServer, closeServer} = require('../server');
+
+const {TEST_DATABASE_URL} = require('../config');
+
+chai.use(chaiHttp);
+
+describe('start up test', function() {
+
+  before(function() {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  // beforeEach(function() {
+  //   return seedBlogData();
+  // });
+  //
+  // // afterEach(function() {
+  // //   return tearDownDb();
+  // // });
+
+  after(function() {
+    return closeServer();
+  });
+
+  describe('get endpoint', function() {
+    it('should return status 200', function() {
+      return chai.request(app)
+        .get('/')
+        .then(function(res) {
+          expect(res).to.have.status(200);
+        });
+    });
+  });
+
+});
