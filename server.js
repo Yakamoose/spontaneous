276297b66f852671b47b556ce88ef6386add4cfd
Eric J <xericx.johnson@gmail.com>
2018-03-31 14:31:21 -0700

    trying to fix travis issue

--- server.js
+++ server.js
@@ -46,8 +46,8 @@ function closeServer() {
   });
 }
 
-if (require.main === module) {
-  runServer(DATABASE_URL).catch(err => console.error(err));
-}
-
-module.exports = { app, runServer, closeServer };
+// if (require.main === module) {
+//   runServer(DATABASE_URL).catch(err => console.error(err));
+// }
+//
+// module.exports = { app, runServer, closeServer };
