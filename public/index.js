1a65ecc22de6ccc9fd2611e6f79ca2e71334a819
Eric J <xericx.johnson@gmail.com>
2018-04-02 15:46:13 -0700

    added pages

--- public/index.js
+++ public/index.js
@@ -2,8 +2,22 @@
 function watchSubmit() {
     $('.start-btn').submit(event => {
       event.preventDefault();
-      window.location = 'create-order-page.html';
+      $('.description').hide();
+      $(".sign-up").show();
+      // window.location = 'create-order-page.html';
     });
 }
+
+
+
+function watchLogin() {
+  $('.login').click(event => {
+    event.preventDefault();
+    $('.description').hide();
+    $('.sign-up').show();
+  });
+}
+
+$(watchLogin);
 
 $(watchSubmit);
